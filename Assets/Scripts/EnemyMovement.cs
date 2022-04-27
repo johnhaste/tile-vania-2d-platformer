7f649b2f11de8d86d3aacd04c8b6362d9ff18312
John Haste <joaovictorspteixeira@gmail.com>
2022-04-27 00:49:21 -0300

    Basic Kinematic Enemy Goober

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidBody;
+
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+    }
+}
