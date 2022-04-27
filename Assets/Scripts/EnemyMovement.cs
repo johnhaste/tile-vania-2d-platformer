5bb4786b7af9fc31b47bc39bdee69c737904cd4f
John Haste <joaovictorspteixeira@gmail.com>
2022-04-27 01:19:36 -0300

    Enemy Bounces Off Wall

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,15 +6,27 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
 
 
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
         myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing(){
+        //Flip Horizontally
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
     }
 }
